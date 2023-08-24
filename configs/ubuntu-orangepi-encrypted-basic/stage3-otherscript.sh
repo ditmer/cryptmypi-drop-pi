febd9067d076367747ebd0f298e90b06f48a9f9c
ditmer <rdditmer3@gmail.com>
2023-08-24 10:12:00 -0600

    Add stage3 other scripts hook

---
+++ configs/ubuntu-orangepi-encrypted-basic/stage3-otherscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+###################################
+# Ubuntu encrypted stage3-otherscript.sh
+#
+#  Everything in here is executed within the chroot. No need to call it in here
+#  Use this to install wifi, set wlan configs, add more users, install other things?
