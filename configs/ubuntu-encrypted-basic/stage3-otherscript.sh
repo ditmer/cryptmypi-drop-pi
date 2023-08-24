abb05a88c4890dab7cf0a9c49ebc7f4328247e1a
ditmer <rdditmer3@gmail.com>
2023-08-24 10:14:21 -0600

    Fix my mistakes

--- configs/ubuntu-encrypted-basic/stage3-otherscript.sh
+++ configs/ubuntu-encrypted-basic/stage3-otherscript.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
 
 ###################################
-# Ubuntu encrypted stage2-otherscript.sh
-
-
-# At the time of testing the Ubuntu for rpi works on labels, so we attempt to match.
-#echo 'Setting up partition labels for Ubuntu.'
-#dosfslabel ${_BLKDEV}1 system-boot
-
-
-# https://cryptsetup-team.pages.debian.net/cryptsetup/README.initramfs.html#cryptopts-boot-argument
-#echo 'Setting up /boot/cmdline.txt for encrypted boot with Ubuntu.'
-#sed -i.bak "s#root=LABEL=writable#root=/dev/mapper/crypt#g" /boot/cmdline.txt
-
-
-#echo 'Drop the initramfs entry we made in /boot/config.txt'
-#sed -i.bak "s#initramfs initramfs.gz followkernel##g" /boot/config.txt
-
-
-#echo 'Updating partition information in /etc/fstab for enctyped boot.'
-#sed -i 's#LABEL=writable#/dev/mapper/crypt#g' /etc/fstab
-
-
-# Move our kernel in place of the targets default kernel
-__UBUNTU_KERNEL="initrd.img-5.15.0-1034-raspi"
-echo "Movinng our /boot/initramfs.gz to /boot/${__UBUNTU_KERNEL}."
-mv "/boot/${__UBUNTU_KERNEL}" "/boot/${__UBUNTU_KERNEL}-oos"
-cp /boot/initramfs.gz /boot/initrd.img
-cp /boot/initramfs.gz "/boot/${__UBUNTU_KERNEL}"
-
-if id "ubuntu" >/dev/null 2>&1; then
-    echo "Removing user Ubuntu"
-    userdel -r ubuntu
-fi
+# Ubuntu encrypted stage3-otherscript.sh
+#
+#  Everything in here is executed within the chroot. No need to call it in here
+#  Use this to install wifi, set wlan configs, add more users, install other things?
